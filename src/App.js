3e9d03ccd89bf2e45a36572ec17ffba5de9461d2
riandannyraka <riandannyraka@gmail.com>
2022-04-15 22:49:04 +0700

    Tyringout react-router v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import React from 'react'
+import { useRoutes, Navigate, useNavigate } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 
 function App() {
-  return (
-    <Login />
-  );
+  let element = useRoutes([
+    // These are the same as the props you provide to <Route>
+    { path: "/", element: <Navigate to="/login" /> },
+    { path: "/login", element: <Login navigate={useNavigate()} /> },
+    { path: "/home", element: <Home /> },
+    // { path: "dashboard", element: <Dashboard /> },
+    // {
+    //   path: "Groups",
+    //   element: <Groups />,
+    //   // Nested routes use a children property, which is also
+    //   // the same as <Route>
+    //   children: [
+    //     { path: ":id", element: <Group /> },
+    //     { path: "user", element: <UserGroup /> }
+    //   ]
+    // },
+    // // Not found routes work as you'd expect
+    // { path: "*", element: <NotFound /> }
+  ]);
+  return element
 }
 
 export default App;
